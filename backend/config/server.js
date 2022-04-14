476b62603776f11f96b55e63d31c9fcc9aa0bc1f
labanquise-kevin <kevin.lanvin@ext.adeo.com>
2022-04-14 20:27:53 +0200

    First commit

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '0fe50d4c6f399894685d8ca3eebba511'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '6fd1c18a-9487-424b-b4c8-84c5bdc60f0f'
-};
